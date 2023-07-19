a4110e70d0eb47181237dc1f1adf6d450db5727b
Sebastian Piazza <sebastian@adermarkarian23gmail.onmicrosoft.com>
2023-07-18 21:37:40 -0300

    add quality gate validation to sonar

--- .github/scripts/mavenAnalyze.sh
+++ .github/scripts/mavenAnalyze.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./mvnw -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+./mvnw -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.qualitygate.wait=true -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
